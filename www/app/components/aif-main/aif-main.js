b7557fdf72c2499a36a66f9978799e37f876957a
Kyle Buchanan <kylebuch8@gmail.com>
2014-11-01 16:07:35 -0400

    updates to main screen

--- www/app/components/aif-main/aif-main.js
+++ www/app/components/aif-main/aif-main.js
@@ -5,19 +5,32 @@
     Polymer('aif-main', {
         people: [
             {
-                name: 'Name 1'
+                name: 'Kyle Buchanan',
+                profilePic: 'https://lh5.googleusercontent.com/-1zEfFKV5gKs/AAAAAAAAAAI/AAAAAAAAAAA/mCgpiWGqnB8/s400-c/photo.jpg'
             },
             {
-                name: 'Name 2'
+                name: 'Kristy Buchanan',
+                profilePic: 'https://lh5.googleusercontent.com/-x7z_-JzOPGY/AAAAAAAAAAI/AAAAAAAAiKs/SbHWJpwy8dQ/s400-c/photo.jpg'
             },
             {
-                name: 'Name 3'
+                name: 'James Tysinger',
+                profilePic: 'https://lh5.googleusercontent.com/-azBXZ_uiRKM/AAAAAAAAAAI/AAAAAAAAADg/6YSas88Q93U/s400-c/photo.jpg'
             },
             {
-                name: 'Name 4'
+                name: 'Pat Dungan',
+                profilePic: 'https://lh5.googleusercontent.com/-447WhzYP-xg/AAAAAAAAAAI/AAAAAAAA9C0/apc_GIZTl98/s400-c/photo.jpg'
             },
             {
-                name: 'Name 5'
+                name: 'Luke Dary',
+                profilePic: 'https://lh6.googleusercontent.com/-BjpLFp78GGg/AAAAAAAAAAI/AAAAAAAAYjw/26QXmHETJmU/s400-c/photo.jpg'
+            },
+            {
+                name: 'Michael Roberto',
+                profilePic: 'https://lh6.googleusercontent.com/-VejIBdLSe4E/AAAAAAAAAAI/AAAAAAAADOY/fIdskbsYhk0/s400-c/photo.jpg'
+            },
+            {
+                name: 'Bill Rowland',
+                profilePic: 'https://lh5.googleusercontent.com/-0i8nM4qfO7o/AAAAAAAAAAI/AAAAAAAAMMc/aMC1qhlJIQ0/s400-c/photo.jpg'
             }
         ],
 
